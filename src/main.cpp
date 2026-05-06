146f61f77fcff16fc8f840d8d372ba3f9c124b77
Sandip Dahal <er.sandipdahal@gmail.com>
2026-05-06 18:46:24 +0545

    tweak controls

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,14 @@
 AF_DCMotor leftMotor(1);
 AF_DCMotor rightMotor(2);
 
-const uint8_t irPin = A2;                   // IR sensor pin
-const uint8_t trigPin = A3;                 // US trig pin
-const uint8_t echoPin = A4;                 // US echo pin
-const uint8_t MOTOR_SPEED = 125;            // 0 - 255
-const uint8_t MIN_CLEARANCE = 20;           // cm
-const unsigned long CHECK_INTERVAL = 60;    // ms
-const unsigned long RECOVER_INTERVAL = 100; // ms
+const uint8_t irPin = A2;                  // IR sensor pin
+const uint8_t trigPin = A3;                // US trig pin
+const uint8_t echoPin = A4;                // US echo pin
+const uint8_t MOTOR_SPEED = 125;           // 0 - 255
+const uint8_t MIN_CLEARANCE = 20;          // cm
+const unsigned long CHECK_INTERVAL = 60;   // ms
+const unsigned long RECOVER_DELAY = 150;   // ms
+const unsigned long BACK_AWAY_DELAY = 500; // ms
 
 float distance;
 
@@ -27,12 +28,6 @@ void setup()
   pinMode(trigPin, OUTPUT);
 
   obstacleCheckTimestamp = millis() - 2 * CHECK_INTERVAL;
-
-  leftMotor.setSpeed(MOTOR_SPEED);
-  rightMotor.setSpeed(MOTOR_SPEED);
-
-  leftMotor.run(FORWARD);
-  rightMotor.run(FORWARD);
 }
 
 void loop()
@@ -50,9 +45,6 @@ void loop()
     digitalWrite(trigPin, LOW);
 
     distance = (pulseIn(echoPin, HIGH) * .0343) / 2;
-
-    Serial.print("distance: ");
-    Serial.println(distance);
   }
 
   floorPresent = digitalRead(irPin) == LOW;
@@ -66,39 +58,46 @@ void loop()
 
     // GO
     leftMotor.setSpeed(MOTOR_SPEED);
-    rightMotor.setSpeed(MOTOR_SPEED);
-
     leftMotor.run(FORWARD);
+
+    rightMotor.setSpeed(MOTOR_SPEED);
     rightMotor.run(FORWARD);
 
     motorsRunning = true;
+
+    return;
   }
-  else
+
+  if (!floorPresent && motorsRunning)
   {
-    if (!motorsRunning)
-    {
-      return;
-    }
+    // BREAK!!
+    leftMotor.setSpeed(MOTOR_SPEED * 2);
+    leftMotor.run(BACKWARD);
 
-    if (!floorPresent)
-    {
-      // BREAK!!
-      leftMotor.setSpeed(MOTOR_SPEED * 2);
-      rightMotor.setSpeed(MOTOR_SPEED * 2);
+    rightMotor.setSpeed(MOTOR_SPEED * 2);
+    rightMotor.run(BACKWARD);
 
-      leftMotor.run(BACKWARD);
-      rightMotor.run(BACKWARD);
+    delay(RECOVER_DELAY);
+  }
 
-      delay(RECOVER_INTERVAL);
-    }
-
+  if (motorsRunning)
+  {
     // STOP
     leftMotor.setSpeed(0);
-    rightMotor.setSpeed(0);
-
-    leftMotor.run(RELEASE);
     rightMotor.run(RELEASE);
 
-    motorsRunning = false;
+    rightMotor.setSpeed(0);
+    rightMotor.run(RELEASE);
   }
+
+  if (floorPresent)
+  {
+    // BACK AWAY
+    leftMotor.setSpeed(MOTOR_SPEED);
+    leftMotor.run(BACKWARD);
+
+    delay(BACK_AWAY_DELAY);
+  }
+
+  motorsRunning = false;
 }
